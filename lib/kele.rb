e69039041c398301224fcb135f9a6bba3856ef4a
Craig Schotch II <caschotchii@gmail.com>
2016-11-22 01:36:01 +0000

    Completed retrieval of current user info

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
     include HTTParty
@@ -10,6 +11,11 @@ class Kele
         #binding.pry
         raise "Invalid email/password" if response.code != 200
         @auth_token = response["auth_token"]
+    end
+    
+    def get_me
+       response = self.class.get(api_url('users/me'), headers: { "authorization" => @auth_token })
+       @user_data = JSON.parse(response.body)
     end
 
     private
